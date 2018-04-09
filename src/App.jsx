03e954415e538d278a2bc71c9a82a3453d1b137d
enkhee-Osiris <enkhee.ag@gmail.com>
2018-04-09 16:42:10 +0800

    Fix eslint error

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        aaaaaaaaaaaaaa
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+}
+
+export default App;
