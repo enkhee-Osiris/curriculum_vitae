7244abf9ba7c44bdcaf8c6e69e3f75c71f5bfbe2
enkhee-Osiris <enkhee.ag@gmail.com>
2018-04-09 19:42:42 +0800

    update folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
       </header>
       <p className="App-intro">
-        aaaaaaaaaaaaaa
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
     </div>
